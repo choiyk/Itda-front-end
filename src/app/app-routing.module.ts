792b0281225bd2911a1b267ddbf72960569c74cf
choiyk <d_pvmf@naver.com>
2018-09-14 23:21:43 +0900

    main-event

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { MeetingComponent } from './pages/meeting/meeting.component';
+import { MainComponent } from './pages/main/main.component';
 
 const routes: Routes = [
   {
     path: 'meeting',
     component: MeetingComponent
   },
+  {
+    path: '',
+    component: MainComponent
+  }
 ];
 
 @NgModule({
