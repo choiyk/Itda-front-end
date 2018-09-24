981cc7138c6c3821351fd6556ef6e20935d4c3da
choiyk <d_pvmf@naver.com>
2018-09-24 18:38:15 +0900

    edit main

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,25 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { MeetingComponent } from './pages/meeting/meeting.component';
 import { MainComponent } from './pages/main/main.component';
+import { JobInfoComponent } from './pages/job-info/job-info.component';
+import { EventComponent } from './pages/event/event.component'; 
 
 const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent
+  },
   {
     path: 'meeting',
     component: MeetingComponent
   },
   {
-    path: '',
-    component: MainComponent
+    path: 'jobInfo',
+    component: JobInfoComponent
+  },
+  {
+    path: 'event',
+    component: EventComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { MeetingComponent } from './pages/meeting/meeting.component';
 import { MenuComponent } from './ui/menu/menu.component';
 import { MainComponent } from './pages/main/main.component';
+import { JobInfoComponent } from './pages/job-info/job-info.component';
+import { EventComponent } from './pages/event/event.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MeetingComponent,
     MenuComponent,
-    MainComponent
+    MainComponent,
+    JobInfoComponent,
+    EventComponent
   ],
   imports: [
     BrowserModule,
